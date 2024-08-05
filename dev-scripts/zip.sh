2750139532365fc7e6bae4fab213a882c2e32bf5
willow <brmagrini@gmail.com>
2024-08-05 12:02:19 -0300

    Remove zip do tema antes de criar o novo

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -2,4 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
+rm -f ../zips/hacklab-theme.zip
 zip -r ../zips/hacklab-theme.zip hacklab-theme -x "hacklab-theme/node_modules/*"
