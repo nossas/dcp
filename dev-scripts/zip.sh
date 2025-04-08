a43412febfdc157c03a76d101574d04d3d7c8504
Thayná Zacharias <thayna.zacharias@hacklab.com.br>
2025-04-08 08:44:13 -0300

    cria repositório

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
-rm -f ../zips/hacklab-theme.zip
-zip -r ../zips/hacklab-theme.zip hacklab-theme -x "hacklab-theme/node_modules/*"
+rm -f ../zips/nossas-dcp.zip
+zip -r ../zips/nossas-dcp.zip nossas-dcp -x "nossas-dcp/node_modules/*"
