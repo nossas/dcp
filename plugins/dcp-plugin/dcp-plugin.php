afcbdd47315ea3392b3115febf3a045ddce387d8
willow <brmagrini@gmail.com>
2025-04-18 22:20:04 -0300

    adiciona outros endpoints na api para teste do bot

--- plugins/dcp-plugin/dcp-plugin.php
+++ plugins/dcp-plugin/dcp-plugin.php
@@ -80,3 +80,137 @@ function dcp_get_riscos() {
 
     return rest_ensure_response($riscos);
 }
+
+
+add_action('rest_api_init', function () {
+    register_rest_route('dcp/v1', '/abrigos', [
+        'methods' => 'GET',
+        'callback' => 'dcp_api_abrigos',
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+function dcp_api_abrigos($request) {
+    $abrigos = [
+        [
+            'id' => 1,
+            'nome' => 'Igreja Padre Nelson',
+            'endereco' => 'Rua Projetada A, 120 - Jacarezinho, Japeri - RJ',        
+        ],
+        [
+            'id' => 2,
+            'nome' => 'Igreja Batista',
+            'endereco' => 'Av. Principal, 980 - Jacarezinho, Japeri - RJ',
+        ],
+        [
+            'id' => 3,
+            'nome' => 'Centro cultural Jacarézinho',
+            'endereco' => 'Rua da Paz, 789 - Vila Nova',
+        ],
+    ];
+
+    return rest_ensure_response($abrigos);
+}
+add_action('rest_api_init', function () {
+    register_rest_route('dcp/v1', '/dicas', [
+        'methods' => 'GET',
+        'callback' => 'dcp_api_dicas',
+        'args' => [
+            'tipo' => [
+                'required' => true,
+                'validate_callback' => function($param) {
+                    return in_array($param, ['enchente', 'lixo', 'calor']);
+                },
+            ],
+        ],
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+function dcp_api_dicas($request) {
+    $tipo = $request->get_param('tipo');
+
+    $dicas = [
+        'enchente' => [
+            'Evite contato com a água da enchente.',
+            'Desligue a energia elétrica ao sinal de alagamento.',
+            'Mantenha documentos e objetos importantes em locais altos.',
+        ],
+        'lixo' => [
+            'Não jogue lixo nas ruas ou em bueiros.',
+            'Separe resíduos recicláveis e orgânicos.',
+            'Mantenha o lixo tampado para evitar proliferação de doenças.',
+        ],
+        'calor' => [
+            'Beba bastante água durante o dia.',
+            'Evite exposição ao sol entre 10h e 16h.',
+            'Use roupas leves e protetor solar.',
+        ],
+    ];
+
+    return rest_ensure_response($dicas[$tipo]);
+}
+
+add_action('rest_api_init', function () {
+    register_rest_route('dcp/v1', '/contatos', [
+        'methods' => 'GET',
+        'callback' => 'dcp_api_contatos',
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+function dcp_api_contatos($request) {
+    $contatos = [
+        [
+            'nome' => 'Defesa Civil',
+            'telefone' => '199',
+            'descricao' => 'Atendimento a emergências e desastres naturais',
+        ],
+        [
+            'nome' => 'Corpo de Bombeiros',
+            'telefone' => '193',
+            'descricao' => 'Combate a incêndios e resgate de vítimas',
+        ],
+        [
+            'nome' => 'Posto de Saúde Central',
+            'telefone' => '(21) 9999-8888',
+            'descricao' => 'Atendimento médico de urgência em JCarezinho',
+        ],
+        [
+            'nome' => 'Polícia Militar',
+            'telefone' => '190',
+            'descricao' => 'Emergências de segurança pública',
+        ],
+    ];
+
+    return rest_ensure_response($contatos);
+}
+
+add_action('rest_api_init', function () {
+    register_rest_route('dcp/v1', '/risco-regiao', [
+        'methods' => 'GET',
+        'callback' => 'dcp_api_risco_regiao',
+        'args' => [
+            'regiao' => [
+                'required' => true,
+                'validate_callback' => function($param) {
+                    return is_string($param) && strlen($param) > 1;
+                },
+            ],
+        ],
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+function dcp_api_risco_regiao($request) {
+
+    $minuto = intval(date('s')); 
+    $risco_index = $minuto % 3;
+
+    $graus = ['Baixo', 'Médio', 'Alto'];
+    $grau_risco = $graus[$risco_index];
+
+    return rest_ensure_response([
+        'grau_risco' => $grau_risco,
+    ]);
+}
