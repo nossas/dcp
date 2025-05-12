95c79766ec6967b96945483e77ff4784675f1fd3
dioliviers <dioliviers@gmail.com>
2025-05-12 15:32:29 -0300

    ajuste nos filtros e refatoração

--- themes/dcp/template-parts/filter-apoio-search.php
+++ themes/dcp/template-parts/filter-apoio-search.php
@@ -9,14 +9,22 @@
 
     <div class="apoio-tags">
         <button class="tag-button active" data-tag="all">Todos</button>
-        <?php
-        $terms = get_terms([
-            'taxonomy' => 'tipo_apoio',
-            'hide_empty' => false,
-        ]);
 
-        foreach ( $terms as $term ) {
-            echo '<button class="tag-button" data-tag="' . esc_attr( $term->slug ) . '">' . esc_html( $term->name ) . '</button>';
+        <?php
+        // Pega o termo pai "QUEM ACIONAR"
+        $parent_term = get_term_by('name', 'QUEM ACIONAR', 'tipo_apoio');
+
+        if ($parent_term && !is_wp_error($parent_term)) {
+            // Pega os filhos do termo "QUEM ACIONAR"
+            $child_terms = get_terms([
+                'taxonomy'   => 'tipo_apoio',
+                'parent'     => $parent_term->term_id,
+                'hide_empty' => false,
+            ]);
+
+            foreach ( $child_terms as $term ) {
+                echo '<button class="tag-button" data-tag="' . esc_attr( $term->slug ) . '">' . esc_html( $term->name ) . '</button>';
+            }
         }
         ?>
     </div>
