5af86947661a7bd96efd87a9781144307d19830e
Filipe de Souza <filipe.souza@hacklab.com.br>
2025-05-26 16:04:06 -0300

    aplica breadcrumb nas páginas ref.: #9

--- themes/dcp/page.php
+++ themes/dcp/page.php
@@ -1,15 +1,20 @@
 <?php
 get_header();
 ?>
-    <div class="container">
-        <div class="post-header post-header__separator">
-            <!-- <h1 class="post-header__title"><?php the_title() ?></h1>
+<div class="container">
+    <div class="post-header post-header__separator">
+        <!-- <h1 class="post-header__title"><?php the_title() ?></h1>
             <div class="post-header__excerpt container--wide">
                 <?php the_excerpt() ?>
             </div> -->
-        </div>
     </div>
-    <div class="post-content content content--wide">
-        <?php the_content() ?>
-    </div>
+</div>
+<div class="post-content content content--wide">
+    <?php if (function_exists('bcn_display')) : ?>
+        <nav class="breadcrumb" typeof="BreadcrumbList" vocab="https://schema.org/">
+            <?php bcn_display(); ?>
+        </nav>
+    <?php endif; ?>
+    <?php the_content() ?>
+</div>
 <?php get_footer();
