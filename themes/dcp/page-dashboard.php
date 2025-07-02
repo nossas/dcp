8e3c453c1da208340fbdb69e123ba3ab9fdd485f
diegosanches <diegosanches@Diegos-iMac.local>
2025-07-02 15:07:13 -0300

    add og:image cover para o site + dashboard

--- themes/dcp/page-dashboard.php
+++ themes/dcp/page-dashboard.php
@@ -14,6 +14,8 @@ namespace hacklabr\dashboard;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= get_dashboard_title() ?> | <?= get_bloginfo('name') ?></title>
+    <meta property="og:image" content="<?= get_template_directory_uri() ?>/assets/images/cover-dashboard.png">
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
