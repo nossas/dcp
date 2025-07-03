fc12bf1117a87045f0d1b6c7fa1749996b7dfd27
dioliviers <dioliviers@gmail.com>
2025-07-02 21:34:20 -0300

    insere banner inscrição na single

--- themes/dcp/single.php
+++ themes/dcp/single.php
@@ -54,8 +54,29 @@ if (!file_exists($icon_path)) {
 
 <main class="post-content container container--medium">
     <?php the_content(); ?>
+    <div class="contact-form-single"></div>
+    <div class="wp-block-cover alignfull is-light ficou-alguma-duvida" style="display: flex;">
+    <span aria-hidden="true" class="wp-block-cover__background has-secondary-light-background-color has-background-dim-100 has-background-dim"></span>
+
+    <div class="wp-block-cover__inner-container is-layout-constrained wp-block-cover-is-layout-constrained wp-block-columns is-layout-flex wp-container-core-columns is-layout-28f84493 wp-block-columns is-layout-flex" style="display: flex;">
+
+        <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow" style="flex-basis: 656px;">
+        <figure class="wp-block-image size-full">
+        <img decoding="async" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/banner-duvida.png'); ?>" alt="Descrição da imagem" style="object-fit: cover;">        </figure>
+        <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+        </div>
+
+        <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow" style="flex-basis: 676px;">
+        <?php echo do_shortcode('[contact-form-7 id="76b0509" title="contato"]'); ?>
+        </div>
+
+        <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+    </div>
+</div>
+
 </main>
 
 <?php get_template_part('template-parts/content/related-posts'); ?>
+
 
 <?php get_footer(); ?>
