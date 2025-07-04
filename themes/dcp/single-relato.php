44b48527ea3e9d2fd07d321940d0365c705b218d
dioliviers <dioliviers@gmail.com>
2025-07-04 12:11:31 -0300

    ajuste na single de relatos

--- themes/dcp/single-relato.php
+++ themes/dcp/single-relato.php
@@ -26,7 +26,6 @@
 
 
             <h1 class="relato-titulo"><?php the_title(); ?></h1>
- <!-- Categoria acima do título -->
             <div class="relato-categoria">
                 <?php
                     $terms = get_the_terms(get_the_ID(), 'tipo_acao');
@@ -55,12 +54,15 @@
         <p class="form-descricao">Aenean egestas ultricies nibh, at tempus purus fringilla in. Curabitur ornare enim justo, at tristique.</p>
 
         <form class="form-participar" action="#" method="post">
+            <p>Nome</p>
             <input type="text" name="nome" placeholder="Digite seu nome" required>
+            <p> Telefone (WhatsApp) </p>
             <input type="text" name="telefone" placeholder="(xx) xxxxx-xxxx" required>
+            <p> E-mail (opcional) </p>
             <input type="email" name="email" placeholder="exemplo@email.com">
 
             <label class="checkbox-aceite">
-                <input type="checkbox" name="aceita">
+                <input type="radio" name="aceita">
                 Aceito receber confirmações e informações sobre esta ação via e-mail e WhatsApp
             </label>
 
@@ -73,8 +75,9 @@
     <?php endwhile; else : ?>
         <p>Nenhum relato encontrado.</p>
     <?php endif; ?>
+    <hr>
     <?php get_template_part('template-parts/content/related-posts-acao'); ?>
-
+    <p style="margin-bottom: 64px;">Ficou com alguma dúvida? Fale com a gente para saber mais sobre o projeto ou como participar.  <strong style="text-decoration: underline;"> Entre em contato </strong></p>
 </main>
 
 <?php get_footer(); ?>
