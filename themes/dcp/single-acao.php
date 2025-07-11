3f4f7447696f876033a0c1930cde8432e46586a7
dioliviers <dioliviers@gmail.com>
2025-07-11 16:36:51 -0300

    ajuste da single de ação

--- themes/dcp/single-acao.php
+++ themes/dcp/single-acao.php
@@ -16,13 +16,20 @@
 
     <section class="relato-grid">
         <div class="relato-thumb">
-            <?php if ( has_post_thumbnail() ) : ?>
-                <div class="relato-thumb-wrapper">
-                    <?php the_post_thumbnail('large'); ?>
+    <?php if ( has_post_thumbnail() ) : ?>
+        <div class="relato-thumb-wrapper">
+            <?php the_post_thumbnail('large'); ?>
+        </div>
+            <?php else: ?>
+                <?php
+                    // Pega o tamanho 'large' do WordPress
+                    $large = wp_get_additional_image_sizes()['large'] ?? ['width' => get_option('large_size_w'), 'height' => get_option('large_size_h')];
+                ?>
+                <div class="relato-thumb-wrapper no-thumb" style="width: <?= esc_attr($large['width']) ?>px; height: <?= esc_attr($large['height']) ?>px; background-color: #eee;">
+                    <!-- Espaço reservado sem imagem -->
                 </div>
             <?php endif; ?>
         </div>
-
         <div class="relato-conteudo">
 
             <h1 class="relato-titulo"><?php the_title(); ?></h1>
