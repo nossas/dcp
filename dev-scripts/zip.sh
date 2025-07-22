45bcded9cb546698f1c58b9e0da0563cbc2355c3
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-07-22 09:08:04 -0300

    merge

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
-rm -rf ../zips/dcp.zip
+rm -f ../zips/dcp.zip
 zip -r ../zips/dcp.zip dcp -x "dcp/node_modules/*"
