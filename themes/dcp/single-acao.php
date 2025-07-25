3567f63a968cb5bfc93f3fe1a53211a2b464e3a1
dioliviers <dioliviers@gmail.com>
2025-07-25 06:53:40 -0300

    git commir refatoração entre css da single relato e acoes

--- themes/dcp/single-acao.php
+++ themes/dcp/single-acao.php
@@ -1,37 +1,43 @@
 <?php get_header(); ?>
 
-<main id="primary" class="site-main container  container--wide">
+<main id="primary" class="site-main container container--wide">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
         $pods = pods( 'acao', get_the_ID() );
     ?>
-    <section class="relato-grid">
-        <?php if ( wp_is_mobile() ) : ?>
-            <nav class="relato-conteudo breadcrumb bread-relato" typeof="BreadcrumbList" vocab="https://schema.org/">
-                <?php if ( function_exists('bcn_display') ) {  bcn_display(); } ?>
-            </nav>
-        <?php endif; ?>
-       <div class="relato-thumb">
-        <?php if ( has_post_thumbnail() ) : ?>
-            <div class="relato-thumb-wrapper">
-                <?php the_post_thumbnail('large'); ?>
-            </div>
-        <?php else: ?>
-            <div class="relato-thumb-wrapper no-thumb" style="background-color: #eee; width: 100%; height: 300px;">
-                <!-- Espaço reservado sem imagem -->
-            </div>
-        <?php endif; ?>
-        </div>
-
-        <div class="relato-conteudo">
-            <?php if (!wp_is_mobile()) : ?>
-                <nav class="relato-conteudo breadcrumb bread-relato" typeof="BreadcrumbList" vocab="https://schema.org/">
-                    <?php if ( function_exists('bcn_display') ) {  bcn_display(); } ?>
+    <section class="acao-grid">
+         <?php if (function_exists('bcn_display')): ?>
+                <nav class="bread-acao-mob" aria-label="Breadcrumb">
+                    <?php bcn_display(); ?>
                 </nav>
             <?php endif; ?>
 
-            <h1 class="relato-titulo"><?=$pods->field( 'titulo' )?></h1>
+        <div class="acao-thumb">
+            <?php if ( has_post_thumbnail() ) : ?>
+                <div class="acao-thumb-wrapper">
+                    <?php the_post_thumbnail('large'); ?>
+                </div>
+            <?php else: ?>
+                <div class="acao-thumb-wrapper no-thumb" style="background-color: #eee; width: 100%; height: 300px;">
+                    <!-- Espaço reservado sem imagem -->
+                </div>
+            <?php endif; ?>
+        </div>
 
-            <div class="relato-categoria">
+        <div class="acao-conteudo">
+             <?php if (function_exists('bcn_display')): ?>
+            <nav class="bread-acao" aria-label="Breadcrumb">
+                <?php bcn_display(); ?>
+            </nav>
+            <?php endif; ?>
+            <h1 class="acao-titulo"><?= esc_html( $pods->field( 'titulo' ) ); ?></h1>
+
+            <?php if (!wp_is_mobile()) : ?>
+                <nav class="acao-conteudo breadcrumb bread-acao" typeof="BreadcrumbList" vocab="https://schema.org/">
+                    <?php if ( function_exists('bcn_display') ) { bcn_display(); } ?>
+                </nav>
+            <?php endif; ?>
+
+            <div class="acao-categoria">
                 <?php
                 $terms = get_the_terms(get_the_ID(), 'tipo_acao');
                 if ($terms && !is_wp_error($terms)) {
@@ -53,39 +59,36 @@
                     } elseif (file_exists($png_path)) {
                         $img_path = $png_uri;
                     } else {
-                        // Imagem padrão caso não encontre nenhuma
                         $img_path = $template_uri . '/assets/images/tipo-acao/default.png';
                     }
 
-                        echo '<span class="badge" style="display: inline-flex; align-items: center; gap: 0.5em;">';
-                        //echo '<img src="' . esc_url($img_path) . '" alt="' . esc_attr($nome) . '" style="width: 1.2em; height: auto;">';
-                        echo '<iconify-icon icon="bi:mic-fill"></iconify-icon>';
-                        echo esc_html($nome);
+                    echo '<span class="badge" style="display: inline-flex; align-items: center; gap: 0.5em;">';
+                    echo '<iconify-icon icon="bi:mic-fill"></iconify-icon>';
+                    echo esc_html($nome);
                     echo '</span>';
                 }
                 ?>
             </div>
 
-            <div class="relato-descricao">
+            <div class="acao-descricao">
                 <?php the_content(); ?>
             </div>
 
-            <ul class="relato-info">
-
+            <ul class="acao-info">
                 <li>
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wrapper.svg" alt="Ícone de endereço" style="width: 1em; vertical-align: middle; margin-right: 0.5em;">
                     Endereço: <?php echo esc_html( $pods->display( 'endereco' ) ); ?>
                 </li>
                 <li>
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pin.svg" alt="Ícone de horário" style="width: 1em; vertical-align: middle; margin-right: 0.5em;">
-                    Dia: <?=date( 'd/m/Y, H:i', strtotime( $pods->field( 'data_e_horario' ) ) )?>
+                    Dia: <?= date( 'd/m/Y, H:i', strtotime( $pods->field( 'data_e_horario' ) ) ); ?>
                 </li>
                 <?php
                 $imagem = $pods->display('imagem_relato');
                 if ( $imagem ) : ?>
                     <li>
-                        <img src="<?php echo esc_url( $imagem ); ?>" alt="Imagem do relato" style="width: 1em; vertical-align: middle; margin-right: 0.5em;">
-                        Imagem Relato
+                        <img src="<?php echo esc_url( $imagem ); ?>" alt="Imagem da ação" style="width: 1em; vertical-align: middle; margin-right: 0.5em;">
+                        Imagem da ação
                     </li>
                 <?php endif; ?>
             </ul>
@@ -94,7 +97,7 @@
                 <h2>Participe:</h2>
                 <p class="form-descricao">Aenean egestas ultricies nibh, at tempus purus fringilla in. Curabitur ornare enim justo, at tristique.</p>
 
-                <form id="formParticiparAcao" class="form-participar" action="javascript:void(0);" method="post" data-action="<?php bloginfo( 'url' );?>/wp-admin/admin-ajax.php">
+                <form id="formParticiparAcao" class="form-participar" action="javascript:void(0);" method="post" data-action="<?php bloginfo( 'url' ); ?>/wp-admin/admin-ajax.php">
                     <p>Nome</p>
                     <input type="text" name="nome_completo" placeholder="Digite seu nome" required>
                     <p> Telefone (WhatsApp) </p>
@@ -110,11 +113,11 @@
                         <input type="radio" name="aceite_termos">
                         Aceito termos de uso e política de privacidade.
                     </label>
-                    <input type="hidden" name="post_id" value="<?=get_the_ID()?>">
+                    <input type="hidden" name="post_id" value="<?= get_the_ID(); ?>">
                     <input type="hidden" name="action" value="form_participar_acao">
 
                     <div style="display: flex; justify-content: space-between">
-                        <div> <!----> </div>
+                        <div></div>
                         <button class="botao-confirmar">
                             <iconify-icon icon="bi:check2"></iconify-icon>
                             &nbsp;
@@ -124,19 +127,18 @@
                     </div>
                 </form>
             </section>
-
         </div>
     </section>
 
     <?php endwhile; else : ?>
-        <p>Nenhum relato encontrado.</p>
+        <p>Nenhuma ação encontrada.</p>
     <?php endif; ?>
 
     <hr class="is-separator">
-    <?php get_template_part('template-parts/content/related-posts-acao' ); ?>
+    <?php get_template_part('template-parts/content/related-posts-acao'); ?>
     <p style="padding: 100px 0;">
         Ficou com alguma dúvida? Fale com a gente para saber mais sobre o projeto ou como participar.
-        <strong style="text-decoration: underline; "> Entre em contato </strong>
+        <strong style="text-decoration: underline;">Entre em contato</strong>
     </p>
 </main>
 
