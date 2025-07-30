f249afc30f0791be0aa8eb48a4ddcb9e534d3468
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-07-30 12:20:29 -0300

    Retira visualizaçao do título iniciativas do jacarezinho

--- themes/dcp/page-quem-acionar.php
+++ themes/dcp/page-quem-acionar.php
@@ -21,13 +21,13 @@ get_header();
             <p style="font-size: 20px;">Selecione um tipo de ocorrência ou descreva o que está acontecendo para saber qual serviço ou órgão pode te ajudar.</p>
         </div>
         <?php get_template_part('template-parts/filter-apoio-search'); ?>
-        <div class="title-card container container--wide">
+        <!-- <div class="title-card container container--wide">
             <span>
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-green.svg" alt="Seta verde" style="height: 25px;" />
                 <p> Iniciativas no Jacarezinho </p>
             </span>
 
-        </div>
+        </div> -->
 
         <!-- Lista de Apoios -->
         <?php get_template_part('template-parts/card-apoio-list'); ?>
