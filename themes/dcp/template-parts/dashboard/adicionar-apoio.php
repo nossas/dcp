9c76a259b3c97fb76ce01cb84ffbe05b76b8b39f
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-07-31 18:16:32 -0300

    página criação de novo apoio andamento

---
+++ themes/dcp/template-parts/dashboard/adicionar-apoio.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace hacklabr\dashboard;
+
+    $all_terms = get_terms([
+        'taxonomy' => 'tipo_apoio',
+        'hide_empty' => false,
+    ]);
+
+?>
+<div id="dashboardApoioSingle" class="dashboard-content">
+    <div class="dashboard-content-breadcrumb">
+        <ol class="breadcrumb">
+            <li>
+                <a href="<?=get_dashboard_url( 'apoio' )?>">Apoio</a>
+                <iconify-icon icon="bi:chevron-right"></iconify-icon>
+            </li>
+            <li>
+                <a href="">Adicionar</a>
+                <iconify-icon icon="bi:chevron-right"></iconify-icon>
+            </li>
+            <li><a href="#/">Novo apoio</a></li>
+        </ol>
+    </div>
+    <header class="dashboard-content-header is-single-new">
+        <h1>Adicionar Apoio</h1>
+    </header>
+    <div class="dashboard-content-single">
+        <form id="apoioSingleForm" class="" method="post" enctype="multipart/form-data" action="javascript:void(0);" data-action="<?php bloginfo( 'url' );?>/wp-admin/admin-ajax.php">
+            <div class="fields is-tipo-apoio">
+                <div class="input-wrap">
+                    <label class="label">Tipo de Apoio</label>
+                    <select id="selectTipoApoio" class="select" name="tipo_apoio" required>
+                        <option value="">SELECIONE UM TIPO DE APOIO</option>
+                        <?php foreach ( $all_terms as $key => $term ) :
+                            if( !$term->parent ) : ?>
+                                <option value="<?=$term->slug?>"><?=$term->name?></option>
+                            <?php endif; endforeach; ?>
+                    </select>
+                    <a class="button is-category">
+                        <?php risco_badge_category( 'sem-categoria', 'SEM CATEGORIA ADICIONADA', '' ); ?>
+                    </a>
+                    <a class="button is-select-input">
+                        <iconify-icon icon="bi:chevron-down"></iconify-icon>
+                    </a>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+
+            <div class="fields is-nome">
+                <div class="input-wrap">
+                    <label class="label">Nome</label>
+                    <input class="input" type="text" name="titulo" placeholder="Digite aqui" value="" disabled required>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+            <div class="fields is-descricao">
+                <div class="input-wrap">
+                    <label class="label">Descrição</label>
+                    <textarea class="textarea" name="descricao" disabled required></textarea>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+            <div class="fields is-funcionamento">
+                <div class="is-group">
+                    <div class="input-wrap">
+                        <label class="label">Dias de funcionamento</label>
+                        <input class="input" type="text" name="dias_funcionamento" placeholder="Digite aqui" value="" disabled required>
+                    </div>
+                    <div class="input-wrap">
+                        <label class="label">Horário de funcionamento</label>
+                        <input class="input" type="time" name="horario" placeholder="Digite aqui" value="" disabled required>
+                    </div>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+            <div class="fields is-endereco">
+                <div class="input-wrap">
+                    <label class="label">Endereço</label>
+                    <input class="input" type="text" name="endereco" placeholder="Digite o local ou endereço aqui" value="" disabled required>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+
+            <div class="fields is-website" style="display: none;">
+                <div class="input-wrap">
+                    <label class="label">Website</label>
+                    <input class="input" type="text" name="website" placeholder="Digite o local ou endereço aqui" value="" required>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+            <div class="fields is-info-extra" style="display: none;">
+                <div class="input-wrap">
+                    <label class="label">Informações extras</label>
+                    <textarea class="textarea" name="info_extra" required></textarea>
+                </div>
+                <div class="input-help">
+                    <a href="#/" class="button">
+                        <iconify-icon icon="bi:question"></iconify-icon>
+                    </a>
+                    <p>
+                        Todos os campos devem ter pelo menos 5 caracteres.
+                    </p>
+                </div>
+            </div>
+            <div class="fields is-media-attachments" style="display: none;">
+                <div id="mediaUploadCover" class="input-media">
+                    <?php if( !wp_is_mobile() ) : ?>
+                        <div class="input-media-uploader">
+                            <h4>Foto de capa</h4>
+                            <div class="input-media-uploader-files">
+                                <a id="mediaUploadButtonCover" class="button is-primary is-small is-upload-media">
+                                    <iconify-icon icon="bi:upload"></iconify-icon>
+                                    <span>Adicionar foto</span>
+                                </a>
+                            </div>
+                        </div>
+                    <?php else : ?>
+                        <div class="input-media-uploader is-mobile-only">
+                            <h4 style="margin-top: 15px">Foto (opcional)</h4>
+                            <div class="input-help">
+                                <a href="#/" class="button" style="top: 0 !important;">
+                                    <iconify-icon icon="bi:question"></iconify-icon>
+                                </a>
+                                <p>
+                                    Todos os campos devem ter pelo menos 5 caracteres.
+                                </p>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <div class="input-media-uploader-progress">
+                        <div class="progress is-empty">
+                            <p class="is-empty-text">Funcionalidade de arrasta e solta ainda não disponível.</p>
+                        </div>
+                    </div>
+                    <div class="input-media-preview">
+                        <div class="input-media-preview-assets is-empty">
+                            <p class="is-empty-text">Nenhuma imagem ou vídeo adicionado ainda.</p>
+                        </div>
+                    </div>
+                    <?php if( wp_is_mobile() ) : ?>
+                        <div class="input-media-uploader">
+                            <div class="input-media-uploader-files">
+                                <a id="mediaUploadButtonCover" class="button is-primary is-small is-upload-media">
+                                    <iconify-icon icon="bi:upload"></iconify-icon>
+                                    <span>Adicionar foto</span>
+                                </a>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <?php if( !wp_is_mobile() ) : ?>
+                    <div class="input-help">
+                        <a href="#/" class="button">
+                            <iconify-icon icon="bi:question"></iconify-icon>
+                        </a>
+                        <p>
+                            Todos os campos devem ter pelo menos 5 caracteres.
+                        </p>
+                    </div>
+                <?php endif; ?>
+            </div>
+
+            <div class="form-submit">
+                <input type="hidden" name="action" value="form_single_apoio_new">
+                <div></div>
+                <div>
+                    <a class="button is-goback" href="<?=get_dashboard_url( 'apoio' )?>/">
+                        <iconify-icon icon="bi:chevron-left"></iconify-icon>
+                        <span>Voltar</span>
+                    </a>
+                    <a class="button is-new">
+                        <iconify-icon icon="bi:check2"></iconify-icon>
+                        <span>Criar Apoio</span>
+                    </a>
+                </div>
+            </div>
+        </form>
+        <?php echo get_template_part('template-parts/dashboard/ui/modal-confirm' ); ?>
+    </div>
+</div>
+
--- themes/dcp/template-parts/dashboard/apoio.php
+++ themes/dcp/template-parts/dashboard/apoio.php
@@ -1,3 +1,8 @@
+<?php
+
+namespace hacklabr\dashboard;
+
+?>
 <div id="dashboardApoio" class="dashboard-content">
     <div class="apoio__container">
         <div class="apoio__header situacao-atual__header">
@@ -8,7 +13,7 @@
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M9.00391 2.25098C9.15309 2.25098 9.29617 2.31024 9.40165 2.41573C9.50714 2.52122 9.56641 2.66429 9.56641 2.81348V8.43848H15.1914C15.3406 8.43848 15.4837 8.49774 15.5892 8.60323C15.6946 8.70872 15.7539 8.85179 15.7539 9.00098C15.7539 9.15016 15.6946 9.29324 15.5892 9.39872C15.4837 9.50421 15.3406 9.56348 15.1914 9.56348H9.56641V15.1885C9.56641 15.3377 9.50714 15.4807 9.40165 15.5862C9.29617 15.6917 9.15309 15.751 9.00391 15.751C8.85472 15.751 8.71165 15.6917 8.60616 15.5862C8.50067 15.4807 8.44141 15.3377 8.44141 15.1885V9.56348H2.81641C2.66722 9.56348 2.52415 9.50421 2.41866 9.39872C2.31317 9.29324 2.25391 9.15016 2.25391 9.00098C2.25391 8.85179 2.31317 8.70872 2.41866 8.60323C2.52415 8.49774 2.66722 8.43848 2.81641 8.43848H8.44141V2.81348C8.44141 2.66429 8.50067 2.52122 8.60616 2.41573C8.71165 2.31024 8.85472 2.25098 9.00391 2.25098V2.25098Z" fill="#F9F3EA" />
                     </svg>
                 </div>
-                <a class="apoio__btn-wpp" href="#">
+                <a class="apoio__btn-wpp" href="<?=get_dashboard_url( 'adicionar-apoio' )?>">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M9.00391 2.25098C9.15309 2.25098 9.29617 2.31024 9.40165 2.41573C9.50714 2.52122 9.56641 2.66429 9.56641 2.81348V8.43848H15.1914C15.3406 8.43848 15.4837 8.49774 15.5892 8.60323C15.6946 8.70872 15.7539 8.85179 15.7539 9.00098C15.7539 9.15016 15.6946 9.29324 15.5892 9.39872C15.4837 9.50421 15.3406 9.56348 15.1914 9.56348H9.56641V15.1885C9.56641 15.3377 9.50714 15.4807 9.40165 15.5862C9.29617 15.6917 9.15309 15.751 9.00391 15.751C8.85472 15.751 8.71165 15.6917 8.60616 15.5862C8.50067 15.4807 8.44141 15.3377 8.44141 15.1885V9.56348H2.81641C2.66722 9.56348 2.52415 9.50421 2.41866 9.39872C2.31317 9.29324 2.25391 9.15016 2.25391 9.00098C2.25391 8.85179 2.31317 8.70872 2.41866 8.60323C2.52415 8.49774 2.66722 8.43848 2.81641 8.43848H8.44141V2.81348C8.44141 2.66429 8.50067 2.52122 8.60616 2.41573C8.71165 2.31024 8.85472 2.25098 9.00391 2.25098V2.25098Z" fill="#F9F3EA" />
                     </svg>
@@ -36,8 +41,7 @@
 
             <div class="apoio__tabs">
                 <?php foreach ($termos as $termo):
-                    $ativo = ($termo_selecionado === $termo->slug || (!$termo_selecionado && $termo === reset($termos))) ? 'ativo' : '';
-                    ?>
+                    $ativo = ($termo_selecionado === $termo->slug || (!$termo_selecionado && $termo === reset($termos))) ? 'ativo' : ''; ?>
                     <a
                         href="?tipo=<?= esc_attr($termo->slug) ?>"
                         class="apoio__tab <?= $ativo ?>">
@@ -45,9 +49,7 @@
                     </a>
                 <?php endforeach; ?>
 
-                <?php
-                $ativo_arquivados = ($termo_selecionado === 'arquivados') ? 'ativo' : '';
-                ?>
+                <?php $ativo_arquivados = ($termo_selecionado === 'arquivados') ? 'ativo' : ''; ?>
                 <a href="?tipo=arquivados" class="apoio__tab <?= $ativo_arquivados ?>">
                     Arquivados
                 </a>
@@ -94,7 +96,7 @@
                                     ]],
                                 ];
 
-                                $sub_query = new WP_Query($query_args);
+                                $sub_query = new \WP_Query($query_args);
 
                                 if ($sub_query->have_posts()) :
                                     echo '<div class="apoio__grupo">';
@@ -138,7 +140,7 @@
                             ];
                         }
 
-                        $apoios_query = new WP_Query($query_args);
+                        $apoios_query = new \WP_Query($query_args);
 
                         if ($apoios_query->have_posts()) :
                             while ($apoios_query->have_posts()) : $apoios_query->the_post();
@@ -149,8 +151,7 @@
                             echo '<p style="padding:1rem;">Nenhum item encontrado.</p>';
                         endif;
 
-                    endif;
-                    ?>
+                    endif; ?>
                 </div>
             </div>
         </div>
