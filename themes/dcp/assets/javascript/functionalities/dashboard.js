cf28483cea41ac8f60c8042920205666d04c8cad
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-08-04 13:24:09 -0300

    add full address

--- themes/dcp/assets/javascript/functionalities/dashboard.js
+++ themes/dcp/assets/javascript/functionalities/dashboard.js
@@ -372,6 +372,7 @@ jQuery(function($) {
                 },
                 success: function( response ) {
                     if( response.lat || response.lon ) {
+                        $this.val( response.address )
                         $( '.dashboard-content-single input[name="latitude"]' ).val( response.lat );
                         $( '.dashboard-content-single input[name="longitude"]' ).val( response.lon );
                         $this.parent().find( '.is-success' ).show();
