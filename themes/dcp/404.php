329202aa122b73869c2b3c52c083d84db0e4ebf7
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-08-11 14:23:25 -0300

    implementa e estiliza página de error 404

--- themes/dcp/404.php
+++ themes/dcp/404.php
@@ -1,12 +1,25 @@
 <?php get_header(); ?>
-<div class="error-404">
-        <h1>
-            <span class="error"><?php _e('error', 'hacklabr') ?></span>
-            <span class="num">404</span>
-        </h1>
-
-        <p><?php _e('Page not found', 'hacklabr') ?></p>
-        <a href="<?= home_url() ?>" class="button"> <span><?php _e('Return to home page', 'hacklabr') ?></span> </a>
+<div class="error-404 container container--wide">
+    <div class="error-404__content">
+        <div class="error-404__image">
+            <img src="<?= get_template_directory_uri() ?>/assets/images/image-404.svg" alt="404">
+        </div>
+        <div class="error-404__error">
+            <h1 class="error-404__error-title">
+                <?php _e('Algo deu errado', 'hacklabr') ?>
+            </h1>
+            <p class="error-404__error-subtitle">
+                <?php _e("Não conseguimos encontrar a página que você procurou. Talvez o link esteja incorreto ou a página tenha sido removida.", 'hacklabr') ?>
+            </p>
+            <a href="<?= home_url() ?>" class="button"> <span><?php _e('Return to home page', 'hacklabr') ?></span> </a>
+            <div class="error-404__error-divider"></div>
+            <div class="error-404__error-wrapper-contact">
+                <span class="error-404__error-contact">
+                    <?php _e('Se o problema persistir, fale com a gente pelo e-mail ', 'hacklabr') ?>
+                    <a href="mailto:contato@defesaclimaticapopular.com<?= get_field('email', 'option') ?>"><?php _e('contato@defesaclimaticapopular.com', 'hacklabr') ?></a>
+                </span>
+            </div>
+        </div>
     </div>
 
 <?php get_footer(); ?>
