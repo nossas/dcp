b2fcaf60c96b99f1e0f19dcec2badf9dfef1f8c8
Filipe de Souza <filipe.souza@hacklab.com.br>
2025-09-05 15:32:40 -0300

    ajuste e remoção de espaçamento Ref.: #113

--- themes/dcp/assets/javascript/functionalities/app.js
+++ themes/dcp/assets/javascript/functionalities/app.js
@@ -252,6 +252,31 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const phoneInputs = document.querySelectorAll('.phone');
+
+  phoneInputs.forEach(function(input) {
+    input.addEventListener('input', function(e) {
+      let value = e.target.value.replace(/\D/g, '');
+
+      if (value.length > 10) {
+        value = value.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3');
+      }
+      else if (value.length > 6) {
+        value = value.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, '($1) $2-$3');
+      }
+      else if (value.length > 2) {
+        value = value.replace(/^(\d{2})(\d{0,5})/, '($1) $2');
+      }
+      else {
+        value = value.replace(/^(\d*)/, '($1');
+      }
+
+      e.target.value = value;
+    });
+  });
+});
+
 
 
 
