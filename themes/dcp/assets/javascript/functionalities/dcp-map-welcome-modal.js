3ebdd8eaf995e745feed032a02a3a1a860a8e816
DGS <dgs@Diegos-iMac.local>
2025-10-06 17:34:33 -0300

    add localstorage modal welcome + ajuste botão mapa

--- themes/dcp/assets/javascript/functionalities/dcp-map-welcome-modal.js
+++ themes/dcp/assets/javascript/functionalities/dcp-map-welcome-modal.js
@@ -1,22 +1,25 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('welcomeModal', () => ({
-    isOpen: false,
-    step: 1,
+    Alpine.data('welcomeModal', () => ({
+        isOpen: false,
+        step: 1,
 
-    init() {
-      this.isOpen = true;
-    },
+        init() {
+            if (!localStorage.getItem('welcomeModal')) {
+                this.isOpen = true;
+            }
+        },
 
-    nextStep() {
-      if (this.step < 3) {
-        this.step++;
-      } else {
-        this.closeModal();
-      }
-    },
+        nextStep() {
+            if (this.step < 3) {
+                this.step++;
+            } else {
+                this.closeModal();
+            }
+        },
 
-    closeModal() {
-      this.isOpen = false;
-    }
-  }));
+        closeModal() {
+            this.isOpen = false;
+            localStorage.setItem('welcomeModal', 'seen');
+        }
+    }));
 });
