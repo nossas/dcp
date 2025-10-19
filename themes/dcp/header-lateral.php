724c009dbdd83b1580ef3c8a26c7fdcc3a331e06
Filipe de Souza <filipe.souza@hacklab.com.br>
2025-10-18 21:36:18 -0300

    aplica botão de login no mobile Ref.: #156

--- themes/dcp/header-lateral.php
+++ themes/dcp/header-lateral.php
@@ -30,14 +30,22 @@
         </div>
     </div>
 
-    <header x-data="{ menuOpen: false, searchOpen: false }" class="main-header main-header-lateral" :class="{ 'main-header-lateral--menu-open': menuOpen, 'main-header-lateral--search-open': searchOpen }">
+    <header x-data="{ menuOpen: false, searchOpen: false }"
+        class="main-header main-header-lateral"
+        :class="{ 'main-header-lateral--menu-open': menuOpen, 'main-header-lateral--search-open': searchOpen }">
+
         <div class="container container--wide">
             <div class="main-header-lateral__content">
-                <button type="button" class="main-header__toggle-menu main-header-lateral__toggle-menu" aria-label="<?= __('Toggle menu visibility', 'hacklabr') ?>" @click="menuOpen = !menuOpen">
+                <!-- Botão menu mobile -->
+                <button type="button"
+                    class="main-header__toggle-menu main-header-lateral__toggle-menu"
+                    aria-label="<?= __('Toggle menu visibility', 'hacklabr') ?>"
+                    @click="menuOpen = !menuOpen">
                     <source media="(max-width: 768px)" srcset="<?= get_template_directory_uri() ?>/assets/images/DCP_Logo_Escura_4.svg">
                     <img src="<?= get_template_directory_uri() ?>/assets/images/toggle.svg" width="200" alt="<?= esc_attr(get_bloginfo('name')) ?>">
                 </button>
 
+                <!-- Logo -->
                 <div class="main-header-lateral__logo">
                     <a href="<?php echo esc_url(home_url('/')); ?>" class="custom-logo-link" rel="home">
                         <picture>
@@ -47,19 +55,39 @@
                     </a>
                 </div>
 
+                <!-- Botão Login (Mobile visível) -->
+                <div class="main-header-lateral__login main-header-lateral__login--mobile">
+                    <a href="<?= wp_login_url(get_permalink()); ?>">
+                        <button class="main-header-lateral__login-access"><?= __('Login') ?></button>
+                    </a>
+                </div>
+
+                <!-- Grid Desktop -->
                 <div class="main-header-lateral__grid">
                     <div class="main-header-lateral__desktop-content">
-                        <?= wp_nav_menu(['theme_location' => 'main-menu', 'container' => 'nav', 'menu_class' => 'menu', 'container_class' => 'main-header-lateral__menu-desktop']) ?>
+                        <?= wp_nav_menu([
+                            'theme_location' => 'main-menu',
+                            'container' => 'nav',
+                            'menu_class' => 'menu',
+                            'container_class' => 'main-header-lateral__menu-desktop'
+                        ]) ?>
                     </div>
 
+                    <!-- Botão Login Desktop -->
                     <div class="main-header-lateral__login">
-                        <a href="<?= wp_login_url( get_permalink() ); ?>"><button class="main-header-lateral__login-access"><?= __('Login') ?></button></a>
+                        <a href="<?= wp_login_url(get_permalink()); ?>">
+                            <button class="main-header-lateral__login-access"><?= __('Login') ?></button>
+                        </a>
                     </div>
                 </div>
 
+                <!-- Pesquisa -->
                 <div class="main-header-lateral__search">
                     <?php get_search_form(); ?>
-                    <button type="button" class="main-header__toggle-search main-header-lateral__toggle-search" aria-label="<?= __('Toggle search form visibility', 'hacklabr') ?>" @click="searchOpen = !searchOpen">
+                    <button type="button"
+                        class="main-header__toggle-search main-header-lateral__toggle-search"
+                        aria-label="<?= __('Toggle search form visibility', 'hacklabr') ?>"
+                        @click="searchOpen = !searchOpen">
                         <iconify-icon icon="fa-solid:search"></iconify-icon>
                     </button>
                 </div>
@@ -67,10 +95,15 @@
                 <?php do_action('hacklabr/header/menus-end'); ?>
             </div>
         </div>
-        </div>
 
+        <!-- Conteúdo do menu mobile -->
         <div class="main-header-lateral__mobile-content">
-            <?= wp_nav_menu(['theme_location' => 'main-menu', 'container' => 'nav', 'menu_class' => 'menu', 'container_class' => 'main-header-lateral__menu-mobile']) ?>
+            <?= wp_nav_menu([
+                'theme_location' => 'main-menu',
+                'container' => 'nav',
+                'menu_class' => 'menu',
+                'container_class' => 'main-header-lateral__menu-mobile'
+            ]) ?>
         </div>
     </header>
 
@@ -78,20 +111,19 @@
         <div class="sub-header__container">
             <div class="sub-header__content-left">
                 <div class="sub-header__icon">
-                    <div class="warning"> <?= __('Atenção') ?> </div>
+                    <div class="warning"><?= __('Atenção') ?></div>
                 </div>
                 <div class="sub-header__content">
-                    <div class="advertment"> <?= __('Alagamento em algumas áreas do Jacarezinho. Evite locais de risco.') ?> </div>
+                    <div class="advertment"><?= __('Alagamento em algumas áreas do Jacarezinho. Evite locais de risco.') ?></div>
                 </div>
             </div>
 
             <div class="sub-header__content-right">
                 <div class="sub-header__read-more">
-                    <a href="#"> <?= __('Saiba mais') ?> </a>
+                    <a href="#"><?= __('Saiba mais') ?></a>
                 </div>
-
                 <div class="sub-header__close">
-                    <a href="#"> <?= __('X') ?> </a>
+                    <a href="#"><?= __('X') ?></a>
                 </div>
             </div>
         </div>
