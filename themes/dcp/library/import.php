9f6118d262254800066bc3ad2ac35b95d3d723c1
leopiccionia <leopiccionia@gmail.com>
2025-10-20 16:58:18 -0300

    Update datetime if zeroed

--- themes/dcp/library/import.php
+++ themes/dcp/library/import.php
@@ -9,7 +9,7 @@ function add_datetime_to_imported_post (int $post_id) {
     if (in_array($post->post_type, ['acao', 'apoio', 'relato', 'risco'])) {
         $datetime = get_post_meta($post_id, 'data_e_horario', true);
 
-        if (empty($datetime)) {
+        if (empty($datetime) || $datetime === '0000-00-00 00:00:00') {
             $datetime = date('Y-m-d H:i:s');
             update_post_meta($post_id, 'data_e_horario', $datetime);
         }
