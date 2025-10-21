081cabf763f087220aa2c6fd3a23a50504eae1ac
Filipe de Souza <filipe.souza@hacklab.com.br>
2025-10-21 18:12:24 -0300

    aplica botões no menu toggle Ref.: #156

--- themes/dcp/header-lateral.php
+++ themes/dcp/header-lateral.php
@@ -20,7 +20,7 @@
                     <?= the_social_networks_menu(false); ?>
                 </div>
                 <div class="acessibilidade">
-                    <a href="#"><iconify-icon icon="material-symbols-light:contrast"></iconify-icon></a>
+                    <a href="#"><iconify-icon icon="material-symbols-light:contrast"><iconify-icon icon="material-symbols-light:contrast"></iconify-icon></a>
                     <a href="#"><iconify-icon icon="mdi:format-font-size-increase"></iconify-icon></a>
                     <a href="#"><iconify-icon icon="mdi:format-font-size-decrease"></iconify-icon></a>
                     <a href="#"><iconify-icon icon="bi:volume-down-fill"></iconify-icon></a>
@@ -30,103 +30,114 @@
         </div>
     </div>
 
-    <header x-data="{ menuOpen: false, searchOpen: false }"
-        class="main-header main-header-lateral"
-        :class="{ 'main-header-lateral--menu-open': menuOpen, 'main-header-lateral--search-open': searchOpen }">
+<header x-data="{ menuOpen: false, searchOpen: false }"
+    class="main-header main-header-lateral"
+    :class="{ 'main-header-lateral--menu-open': menuOpen, 'main-header-lateral--search-open': searchOpen }">
 
-        <div class="container container--wide">
-            <div class="main-header-lateral__content">
-                <!-- Botão menu mobile -->
-                <button type="button"
-                    class="main-header__toggle-menu main-header-lateral__toggle-menu"
-                    aria-label="<?= __('Toggle menu visibility', 'hacklabr') ?>"
-                    @click="menuOpen = !menuOpen">
-                    <source media="(max-width: 768px)" srcset="<?= get_template_directory_uri() ?>/assets/images/DCP_Logo_Escura_4.svg">
-                    <img src="<?= get_template_directory_uri() ?>/assets/images/toggle.svg" width="200" alt="<?= esc_attr(get_bloginfo('name')) ?>">
-                </button>
+    <div class="container container--wide">
+        <div class="main-header-lateral__content">
+            <!-- Botão menu mobile -->
+            <button type="button"
+                class="main-header__toggle-menu main-header-lateral__toggle-menu"
+                aria-label="<?= __('Toggle menu visibility', 'hacklabr') ?>"
+                @click="menuOpen = !menuOpen">
+                <source media="(max-width: 768px)" srcset="<?= get_template_directory_uri() ?>/assets/images/DCP_Logo_Escura_4.svg">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/toggle.svg" width="200" alt="<?= esc_attr(get_bloginfo('name')) ?>">
+            </button>
 
-                <!-- Logo -->
-                <div class="main-header-lateral__logo">
-                    <a href="<?php echo esc_url(home_url('/')); ?>" class="custom-logo-link" rel="home">
-                        <picture>
-                            <img media="(max-width: 900px)" class="main-header-lateral__logo-mobile1" srcset="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/DCP_Logo_Escura_4.svg">
-                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo-defesa-climatica-popular-full.svg" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" class="custom-logo">
-                        </picture>
-                    </a>
+            <!-- Logo -->
+            <div class="main-header-lateral__logo">
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="custom-logo-link" rel="home">
+                    <picture>
+                        <img media="(max-width: 900px)" class="main-header-lateral__logo-mobile1" srcset="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/DCP_Logo_Escura_4.svg">
+                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo-defesa-climatica-popular-full.svg" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" class="custom-logo">
+                    </picture>
+                </a>
+            </div>
+
+            <!-- Grid Desktop -->
+            <div class="main-header-lateral__grid">
+                <div class="main-header-lateral__desktop-content">
+                    <?= wp_nav_menu([
+                        'theme_location' => 'main-menu',
+                        'container' => 'nav',
+                        'menu_class' => 'menu',
+                        'container_class' => 'main-header-lateral__menu-desktop'
+                    ]) ?>
                 </div>
 
-                <!-- Botão Login (Mobile visível) -->
-                <div class="main-header-lateral__login main-header-lateral__login--mobile">
+                <!-- Botão Login Desktop -->
+                <div class="main-header-lateral__login">
                     <a href="<?= wp_login_url(get_permalink()); ?>">
                         <button class="main-header-lateral__login-access"><?= __('Login') ?></button>
                     </a>
                 </div>
+            </div>
 
-                <!-- Grid Desktop -->
-                <div class="main-header-lateral__grid">
-                    <div class="main-header-lateral__desktop-content">
-                        <?= wp_nav_menu([
-                            'theme_location' => 'main-menu',
-                            'container' => 'nav',
-                            'menu_class' => 'menu',
-                            'container_class' => 'main-header-lateral__menu-desktop'
-                        ]) ?>
-                    </div>
+            <!-- Pesquisa -->
+            <div class="main-header-lateral__search">
+                <?php get_search_form(); ?>
+                <button type="button"
+                    class="main-header__toggle-search main-header-lateral__toggle-search"
+                    aria-label="<?= __('Toggle search form visibility', 'hacklabr') ?>"
+                    @click="searchOpen = !searchOpen">
+                    <iconify-icon icon="fa-solid:search"></iconify-icon>
+                </button>
+            </div>
 
-                    <!-- Botão Login Desktop -->
-                    <div class="main-header-lateral__login">
-                        <a href="<?= wp_login_url(get_permalink()); ?>">
-                            <button class="main-header-lateral__login-access"><?= __('Login') ?></button>
-                        </a>
-                    </div>
-                </div>
+            <?php do_action('hacklabr/header/menus-end'); ?>
+        </div>
+    </div>
 
-                <!-- Pesquisa -->
-                <div class="main-header-lateral__search">
-                    <?php get_search_form(); ?>
-                    <button type="button"
-                        class="main-header__toggle-search main-header-lateral__toggle-search"
-                        aria-label="<?= __('Toggle search form visibility', 'hacklabr') ?>"
-                        @click="searchOpen = !searchOpen">
-                        <iconify-icon icon="fa-solid:search"></iconify-icon>
+    <!-- Conteúdo do menu mobile -->
+    <div class="main-header-lateral__mobile-content" x-show="menuOpen" x-transition>
+        <?= wp_nav_menu([
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'menu_class' => 'menu',
+            'container_class' => 'main-header-lateral__menu-mobile'
+        ]) ?>
+
+        <!-- Botão Login -->
+         <div class="main-header-lateral__mobile-btns">
+             <div class="main-header-lateral__login main-header-lateral__login--mobile">
+                 <a href="<?= wp_login_url(get_permalink()); ?>">
+                     <button class="main-header-lateral__login-access"><?= __('Login') ?></button>
+                 </a>
+             </div>
+             <div class="main-header-lateral__adicionar-risco">
+                 <a href="<?= esc_url(home_url('/informar-riscos')); ?>">
+                     <button class="main-header-lateral__adicionar-risco-button dcp-map-block__add-risk">
+                        <iconify-icon icon="bi:geo-alt-fill"></iconify-icon>
+                        <?= __('Adicionar Risco') ?>
                     </button>
-                </div>
+                 </a>
+             </div>
+         </div>
+    </div>
+</header>
 
-                <?php do_action('hacklabr/header/menus-end'); ?>
+
+<div class="sub-header">
+    <div class="sub-header__container">
+        <div class="sub-header__content-left">
+            <div class="sub-header__icon">
+                <div class="warning"><?= __('Atenção') ?></div>
+            </div>
+            <div class="sub-header__content">
+                <div class="advertment"><?= __('Alagamento em algumas áreas do Jacarezinho. Evite locais de risco.') ?></div>
             </div>
         </div>
 
-        <!-- Conteúdo do menu mobile -->
-        <div class="main-header-lateral__mobile-content">
-            <?= wp_nav_menu([
-                'theme_location' => 'main-menu',
-                'container' => 'nav',
-                'menu_class' => 'menu',
-                'container_class' => 'main-header-lateral__menu-mobile'
-            ]) ?>
-        </div>
-    </header>
-
-    <div class="sub-header">
-        <div class="sub-header__container">
-            <div class="sub-header__content-left">
-                <div class="sub-header__icon">
-                    <div class="warning"><?= __('Atenção') ?></div>
-                </div>
-                <div class="sub-header__content">
-                    <div class="advertment"><?= __('Alagamento em algumas áreas do Jacarezinho. Evite locais de risco.') ?></div>
-                </div>
+        <div class="sub-header__content-right">
+            <div class="sub-header__read-more">
+                <a href="#"><?= __('Saiba mais') ?></a>
             </div>
-
-            <div class="sub-header__content-right">
-                <div class="sub-header__read-more">
-                    <a href="#"><?= __('Saiba mais') ?></a>
-                </div>
-                <div class="sub-header__close">
-                    <a href="#"><?= __('X') ?></a>
-                </div>
+            <div class="sub-header__close">
+                <a href="#"><?= __('X') ?></a>
             </div>
         </div>
     </div>
+</div>
 
     <div id="app">
