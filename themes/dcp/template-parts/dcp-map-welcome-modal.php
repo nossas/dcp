0fe1f8a59c9cb9cef3a3c955dd7ba6cbfd4941ed
Filipe de Souza <filipe.souza@hacklab.com.br>
2025-12-05 16:38:09 -0300

    adiciona textos novos aos modais Ref.: no issue

--- themes/dcp/template-parts/dcp-map-welcome-modal.php
+++ themes/dcp/template-parts/dcp-map-welcome-modal.php
@@ -9,8 +9,7 @@
     x-transition:leave-start="opacity-100"
     x-transition:leave-end="opacity-0"
     class="welcome-modal-overlay"
-    style="display: none;"
->
+    style="display: none;">
     <div class="dcp-map-welcome-modal" @click.away="closeModal()">
 
         <div class="dcp-map-welcome-modal__step-content" x-show="step === 1">
@@ -19,19 +18,37 @@
             </div>
             <div class="dcp-map-welcome-modal__text-container">
                 <h3 class="dcp-map-welcome-modal__title"><?= _e('Bem-vindo(a) ao Mapeamento da Defesa Climática Popular!', 'dcp'); ?></h3>
-                <p class="dcp-map-welcome-modal__text"><?= _e('Acompanhe o que acontece no Jacarezinho com informações enviadas por ', 'dcp'); ?> <strong><?= _e('quem vive no território.', 'dcp'); ?></strong></p>
-                <p class="dcp-map-welcome-modal__text"><?= _e('Você verá relatos da comunidade e áreas com maior chance de alagamento ou acúmulo de lixo.', 'dcp'); ?></p>
+                <p class="dcp-map-welcome-modal__text"><?= _e('Explore as camadas de risco de alagamento e os pontos de apoio no território.', 'dcp'); ?></p>
                 <div class="dcp-map-welcome-modal__tags">
-                    <div class="dcp-map-welcome-modal__tag-container alagamento">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/pin-risco-alagamento.svg" alt="Icone de risco de alagamento" class="dcp-map-welcome-modal__tag-icon">
-                        <span class="dcp-map-welcome-modal__tag tag-alagamento"><?= _e('Alagamento', 'dcp'); ?></span>
-                    </div>
-                    <!--
-                    <div class="dcp-map-welcome-modal__tag-container lixo">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/pin-risco-lixo-amarelo.svg" alt="Icone de risco de lixo" class="dcp-map-welcome-modal__tag-icon">
-                        <span class="dcp-map-welcome-modal__tag tag-lixo"><?= _e('Lixo', 'dcp'); ?></span>
-                    </div>
-                    -->
+                    <ul class="dcp-map-legend__list">
+                        <li class="dcp-map-legend__item dcp-map-legend--desktop dcp-map-legend__alagamento-nivel1">
+                            <button type="button" aria-label="<?= __('Exibir/ocultar zona de risco muito baixo de alagamento', 'hacklabr') ?>">
+                                <span class="dcp-map-legend__item--alagamento-nivel1"><?= __('Risco de alagamento: Muito baixo', 'hacklabr') ?></span>
+                            </button>
+                        </li>
+
+                        <li class="dcp-map-legend__item dcp-map-legend--desktop dcp-map-legend__alagamento-nivel2">
+                            <button type="button" aria-label="<?= __('Exibir/ocultar zona de risco baixo de alagamento', 'hacklabr') ?>">
+                                <span class="dcp-map-legend__item--alagamento-nivel2"><?= __('Risco de alagamento: Baixo', 'hacklabr') ?></span>
+                            </button>
+                        </li>
+
+                        <li class="dcp-map-legend__item dcp-map-legend--desktop dcp-map-legend__alagamento-nivel3">
+                            <button type="button" aria-label="<?= __('Exibir/ocultar zona de risco médio de alagamento', 'hacklabr') ?>">
+                                <span class="dcp-map-legend__item--alagamento-nivel3"><?= __('Risco de alagamento: Médio', 'hacklabr') ?></span>
+                            </button>
+                        </li>
+                        <li class="dcp-map-legend__item dcp-map-legend--desktop dcp-map-legend__alagamento-nivel4">
+                            <button type="button" aria-label="<?= __('Exibir/ocultar zona de risco alto de alagamento', 'hacklabr') ?>">
+                                <span class="dcp-map-legend__item--alagamento-nivel4"><?= __('Risco de alagamento: Alto', 'hacklabr') ?></span>
+                            </button>
+                        </li>
+                        <li class="dcp-map-legend__item dcp-map-legend--desktop dcp-map-legend__alagamento-nivel5">
+                            <button type="button" aria-label="<?= __('Exibir/ocultar zona de risco muito alto de alagamento', 'hacklabr') ?>">
+                                <span class="dcp-map-legend__item--alagamento-nivel5"><?= __('Risco de alagamento: Muito alto', 'hacklabr') ?></span>
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -45,10 +62,19 @@
                 <p class="dcp-map-welcome-modal__text"><?= _e('Este mapeamento é uma ferramenta para agir coletivamente. Você pode:', 'dcp'); ?></p>
                 <div class="dcp-map-welcome-modal__list">
                     <div class="dcp-map-welcome-modal__item">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/lupa.svg" alt="lupa" class="dcp-map-welcome-modal__item-icon">
-                        <div class="dcp-map-welcome-modal__item-text-container">
-                            <h4 class="dcp-map-welcome-modal__item-title"><?= _e('Buscar um local', 'dcp'); ?></h4>
-                            <span class="dcp-map-welcome-modal__item-text"><?= _e('Veja o que já foi mapeado na sua rua ou em outras regiões.', 'dcp'); ?></span>
+                        <div class="dcp-map-welcolme-modal__item--container">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/lupa.svg" alt="lupa" class="dcp-map-welcome-modal__item-icon">
+                            <div class="dcp-map-welcome-modal__item-text-container">
+                                <h4 class="dcp-map-welcome-modal__item-title"><?= _e('Buscar um local', 'dcp'); ?></h4>
+                                <span class="dcp-map-welcome-modal__item-text"><?= _e('Veja o que já foi mapeado na sua rua ou em outras regiões.', 'dcp'); ?></span>
+                            </div>
+                        </div>
+                        <div class="dcp-map-welcolme-modal__item--container">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/apoio.svg" alt="apoio" class="dcp-map-welcome-modal__item-icon">
+                            <div class="dcp-map-welcome-modal__item-text-container">
+                                <h4 class="dcp-map-welcome-modal__item-title dcp-map-welcome-modal__item-title-apoio"><?= _e('Ver pontos de apoio', 'dcp'); ?></h4>
+                                <span class="dcp-map-welcome-modal__item-text"><?= _e('Acesse no mapa os locais que oferecem apoio, como serviços públicos e iniciativas comunitárias.', 'dcp'); ?></span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -62,15 +88,15 @@
             <div class="dcp-map-welcome-modal__text-container">
                 <h3 class="dcp-map-welcome-modal__title bold-position"><?= _e('Ajude a manter o mapa vivo!', 'dcp'); ?></h3>
                 <div class="dcp-map-welcome-modal__list">
-                    <div class="dcp-map-welcome-modal__item">
+                    <div class="dcp-map-welcome-modal__item dcp-map-welcome-modal__item-2 ">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/icon1.svg" alt="exclamacao" class="dcp-map-welcome-modal__item-icon icon">
                         <span class="dcp-map-welcome-modal__item-text"><?= _e('Em ', 'dcp'); ?> <strong><?= _e('"O que fazer"', 'dcp'); ?></strong> <?= _e(' você encontra orientações e contatos úteis.', 'dcp'); ?></span>
                     </div>
-                    <div class="dcp-map-welcome-modal__item">
+                    <div class="dcp-map-welcome-modal__item dcp-map-welcome-modal__item-2">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/icon2.svg" alt="interrogacao" class="dcp-map-welcome-modal__item-icon icon">
                         <span class="dcp-map-welcome-modal__item-text"><?= _e('Clique no ', 'dcp'); ?> <strong><?= _e('"?"', 'dcp'); ?></strong> <?= _e(' na barra à esquerda para ver como usar o mapa.', 'dcp'); ?></span>
                     </div>
-                    <div class="dcp-map-welcome-modal__item">
+                    <div class="dcp-map-welcome-modal__item dcp-map-welcome-modal__item-2">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/icon3.svg" alt="faq" class="dcp-map-welcome-modal__item-icon icon">
                         <span class="dcp-map-welcome-modal__item-text"><?= _e('Em caso de emergência, ligue para a Defesa Civil: ', 'dcp'); ?> <strong><?= _e('199.', 'dcp'); ?></strong></span>
                     </div>
@@ -91,13 +117,12 @@
                 class="dcp-map-welcome-modal__button dcp-map-welcome-modal__button--next"
                 :class="{ 'dcp-map-welcome-modal__button--last-step': step === 3 }"
                 @click="nextStep()">
-                    <span x-text="step < 3 ? 'Próximo' : 'Vamos lá!'"></span>
-                    <img
-                        src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/arrow-right.svg"
-                        alt="Avançar"
-                        class="dcp-map-welcome-modal__arrow-icon"
-                        x-show="step < 3"
-                    >
+                <span x-text="step < 3 ? 'Próximo' : 'Vamos lá!'"></span>
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/images/modal-page-map/arrow-right.svg"
+                    alt="Avançar"
+                    class="dcp-map-welcome-modal__arrow-icon"
+                    x-show="step < 3">
             </button>
         </div>
     </div>
