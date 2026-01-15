ae4f1e769a5c9ec926158916d8f35590abf8ff23
Filipe de Souza <filipe.souza@hacklab.com.br>
2026-01-15 17:14:53 -0300

    ajuste de titulo acessível Ref.: #157

--- themes/dcp/single-map.php
+++ themes/dcp/single-map.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <main id="site-content" role="main">
-    <h1 class="sr-only"><?php the_title(); ?></h1>
+    <h2 class="sr-only"><?php the_title(); ?></h2>
     <div class="jeomap map_id_<?php echo esc_attr( $post->ID ); ?>"></div>
 </main>
 <?php get_footer(); ?>
