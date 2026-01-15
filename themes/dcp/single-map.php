bd98a253c3cc274425cc8eab4b654ad1699badf5
Filipe de Souza <filipe.souza@hacklab.com.br>
2026-01-15 18:20:13 -0300

    ajuste de título ref.: no issue

--- themes/dcp/single-map.php
+++ themes/dcp/single-map.php
@@ -1,6 +1,17 @@
 <?php get_header(); ?>
+
 <main id="site-content" role="main">
-    <h2 class="sr-only"><?php the_title(); ?></h2>
-    <div class="jeomap map_id_<?php echo esc_attr( $post->ID ); ?>"></div>
+    <?php
+    $content = get_post_field('post_content', get_the_ID());
+
+    // verifica se já existe um <h1> no conteúdo
+    $has_h1 = (bool) preg_match('/<h1\b[^>]*>/i', $content);
+
+    if ( ! $has_h1 ) : ?>
+        <h1 class="sr-only"><?php the_title(); ?></h1>
+    <?php endif; ?>
+
+    <div class="jeomap map_id_<?php echo esc_attr( get_the_ID() ); ?>"></div>
 </main>
+
 <?php get_footer(); ?>
