494d90e23a6868f7a29d05119a67a77b2a968152
Filipe de Souza <filipe.souza@hacklab.com.br>
2026-01-15 18:28:02 -0300

    retornar o arquivo single map ao estado original Ref.: no issue

--- themes/dcp/single-map.php
+++ themes/dcp/single-map.php
@@ -1,17 +1,6 @@
 <?php get_header(); ?>
-
 <main id="site-content" role="main">
-    <?php
-    $content = get_post_field('post_content', get_the_ID());
-
-    // verifica se já existe um <h1> no conteúdo
-    $has_h1 = (bool) preg_match('/<h1\b[^>]*>/i', $content);
-
-    if ( ! $has_h1 ) : ?>
-        <h1 class="sr-only"><?php the_title(); ?></h1>
-    <?php endif; ?>
-
-    <div class="jeomap map_id_<?php echo esc_attr( get_the_ID() ); ?>"></div>
+    <h1 class="sr-only"><?php the_title(); ?></h1>
+    <div class="jeomap map_id_<?php echo esc_attr( $post->ID ); ?>"></div>
 </main>
-
 <?php get_footer(); ?>
