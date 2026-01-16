44a313f16df98b52e824f9cb52ba25282c564453
leopiccionia <leopiccionia@gmail.com>
2026-01-16 15:22:46 -0300

    Improve positioning of layer announcer (#172)

--- themes/dcp/assets/javascript/shared/legends.js
+++ themes/dcp/assets/javascript/shared/legends.js
@@ -1,6 +1,6 @@
 import { queryHoveredHighlightLayer, toggleMapboxHighlightLayers, toggleMapboxLayers } from './pins'
 
-function updateLevelAnnouncer (level) {
+function updateLevelAnnouncer (level, event) {
     const { levelLabels, themeAssets } = globalThis.hl_dcp_map_data
 
     const announcer = document.querySelector('.dcp-map-selected-layer')
@@ -13,6 +13,8 @@ function updateLevelAnnouncer (level) {
             img.src = `${themeAssets}/assets/images/alagamento-nivel-${level}.svg`
             img.alt = levelLabels[level]
             span.innerText = levelLabels[level]
+            announcer.style.left = event.point.x + 'px'
+            announcer.style.top = event.point.y + 'px'
             announcer.style.display = ''
         } else {
             announcer.style.display = 'none'
@@ -26,7 +28,7 @@ export function setupLegends(jeoMap, selectedLayers) {
     map.on('mousemove', (event) => {
         const selectedLevel = queryHoveredHighlightLayer(map, event)
         toggleMapboxHighlightLayers(map, selectedLevel)
-        updateLevelAnnouncer(selectedLevel)
+        updateLevelAnnouncer(selectedLevel, event)
     })
 
     for (let level = 1; level <= 5; level++) {
